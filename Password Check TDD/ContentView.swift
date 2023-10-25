9eea180e0a53e5c708134f5921612aaeff5c6ccd
Resn4R <purview-joust.0x@icloud.com>
2023-10-25 10:59:11 +0100

    first commit

--- Password Check TDD/ContentView.swift
+++ Password Check TDD/ContentView.swift
@@ -8,12 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Please log in.")
+            
+            Section{
+                VStack  {
+                    TextField("Username: ", text: <#T##Binding<String>#>)
+                    SecureField("Password: ", text: <#T##Binding<String>#>)
+                        .padding(.vertical, 10)
+                    
+                    HStack {
+                        Text("Don't have an account? ")
+                        Button("Sign up.", action: <#T##() -> Void#>)
+                    }
+                }
+            }
         }
         .padding()
     }
---
+++ Password Check TDD/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Password Check TDD
+//
+//  Created by Vito Borghi on 25/10/2023.
+//
+
+import Foundation
+
+struct User {
+    let username: String
+    let password: String
+}
