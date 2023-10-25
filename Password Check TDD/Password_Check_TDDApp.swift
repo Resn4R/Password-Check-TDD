1368416bb5976c5b7a4aa1049583e09bf74dfca3
Resn4R <purview-joust.0x@icloud.com>
2023-10-25 10:45:07 +0100

    Initial Commit

---
+++ Password Check TDD/Password_Check_TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Password_Check_TDDApp.swift
+//  Password Check TDD
+//
+//  Created by Vito Borghi on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Password_Check_TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
